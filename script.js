5554eb409a500d664ebf00e6f375ba4657d15229
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-18 00:25:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var isDate = function (input) {
   //   write your code here
-    var checkDate=/^\d{4}-\d{2}-\d{2}$/.test(input);
-	return checkDate;
+ //    var checkDate=/^\d{4}-\d{2}-\d{2}$/.test(input);
+	// return checkDate;
+	var isDate = function (input) {
+    if (input instanceof Date) return true; // check if input is a Date object
+    
+    var timestamp = Date.parse(input); // try to parse input
+    if (isNaN(timestamp) == false) return true; // check if parsing was successful
+    
+    return false; // if neither condition is met, return false
+};
 	
 };
 
