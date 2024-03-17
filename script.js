a3305920f779e1ef5e440181e8787e0b7364da2f
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-18 00:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var isDate = function (input) {
   //   write your code here
+    var checkDate=/^\d{4}-\d{2}-\d{2}$/.test(input);
+	return checkDate;
+	
 };
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
-alert(isDate(input));
+
+alert(isDate(input));   
+
